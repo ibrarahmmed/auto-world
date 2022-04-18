43b734472ee26bd204a7b0da808685a6bc65eefa
ibrarahmmed <ibrarahmmedhstu@gmail.com>
2022-04-18 16:19:59 +0600

    add header

---
+++ src/component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='nav'>
+            <h2>AUTO WORLD</h2>
+            
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/component/Shop/Shop.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+
+
+    // data load
+
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h3>this for products</h3>
+            </div>
+            <div className="cart-container">
+                <h3>this is for cart</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
